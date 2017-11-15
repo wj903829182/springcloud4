74490ce14122fa5496409f5f2d154bf8e64d3ce6
jack <wj903829182@sina.cn>
2017-11-15 16:30:20 +0800

    Initial commit

---
+++ consumerrocketmq/src/main/java/com/jack/ConsumerrocketmqApplication.java
@@ -0,0 +1,12 @@
+package com.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerrocketmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerrocketmqApplication.class, args);
+	}
+}
---
+++ registercenter/src/main/java/com/jack/RegistercenterApplication.java
@@ -0,0 +1,12 @@
+package com.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistercenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistercenterApplication.class, args);
+	}
+}
