39b711f163358e3123008b2305d35556a07aa6da
jack <wj903829182@sina.cn>
2017-12-27 12:26:39 +0800

    mybatis plus study

---
+++ studymybatisplus2/src/main/java/com/jack/Studymybatisplus2Application.java
@@ -0,0 +1,12 @@
+package com.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Studymybatisplus2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Studymybatisplus2Application.class, args);
+	}
+}
