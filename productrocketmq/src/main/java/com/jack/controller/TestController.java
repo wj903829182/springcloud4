eedb37dca67a745fea61b59717f401536f90f4a9
jack <wj903829182@sina.cn>
2018-01-15 17:12:05 +0800

    通过网关调用rest服务

--- productrocketmq/src/main/java/com/jack/controller/TestController.java
+++ productrocketmq/src/main/java/com/jack/controller/TestController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by jack on 2018/1/15.
  */
@@ -12,9 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("test")
 public class TestController {
     @RequestMapping(value = "/zuulTest",method = RequestMethod.GET)
-    public JSONObject zuulTest(){
+    public JSONObject zuulTest(HttpServletRequest request){
         JSONObject result = new JSONObject();
         result.put("success","通过网关调用成功");
+        System.out.println("zuulTest seesion id is : "+request.getSession().getId());
         return result;
     }
 }
