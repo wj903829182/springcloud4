6f82b3fc0c0b2c89c60037adc4d6bc845441919d
jack <wj903829182@sina.cn>
2018-02-02 11:18:00 +0800

    netty服务器

--- nettyserver/src/main/java/com/jack/NettyserverApplication.java
+++ nettyserver/src/main/java/com/jack/NettyserverApplication.java
@@ -1,12 +1,21 @@
 package com.jack;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NettyserverApplication {
+public class NettyserverApplication implements CommandLineRunner{
+	@Value("${hello}")
+	private String world;
 
 	public static void main(String[] args) {
 		SpringApplication.run(NettyserverApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		System.out.println("hello is "+world);
 	}
 }
